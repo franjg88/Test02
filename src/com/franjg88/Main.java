67b7d4304cffac65d0a95db74dcc9302fc159fb2
franjg <franjg88@hotmail.com>
2018-05-28 23:24:55 +0200

    otro

--- src/com/franjg88/Main.java
+++ src/com/franjg88/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola de nuevo");
+        System.out.println("Otro");
     }
 }
